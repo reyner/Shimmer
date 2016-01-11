edd97b9766da0060f7948f73aeae7c76581a7476
Reyner Crosby <reyner@gmail.com>
2016-01-10 23:17:14 -0800

    Add shared framework scheme

---
+++ Shimmer/Shimmer.h
@@ -0,0 +1,21 @@
+//
+//  Shimmer.h
+//  Shimmer
+//
+//  Created by Reyner Crosby on 1/10/16.
+//  Copyright © 2016 Kimon Tsinteris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shimmer.
+FOUNDATION_EXPORT double ShimmerVersionNumber;
+
+//! Project version string for Shimmer.
+FOUNDATION_EXPORT const unsigned char ShimmerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shimmer/PublicHeader.h>
+
+#import <FBShimmering/FBShimmering.h>
+#import <FBShimmering/FBShimmeringView.h>
+#import <FBShimmering/FBShimmeringLayer.h>
